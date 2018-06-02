d08b764a20b56c2bc31b663fcf32a04467c5397a
tpranav97 <thoratpranav@hotmail.com>
2018-06-02 15:07:15 +0530

    Create PrintList.cpp

---
+++ PrintList.cpp
@@ -0,0 +1,24 @@
+/*
+  Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+void Print(Node *head)
+{
+    Node *temp;
+    temp=head;
+    if(head==NULL)
+        {
+        return;
+    }
+    while(temp!=NULL)
+        {
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
